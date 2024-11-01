7cf68e04fdd837896e8b8d0d1f3dc7a8408f790c
ngxquang <baophong2401@gmail.com>
2024-11-01 10:41:56 +0700

    feat: add hostnameand ecosystem

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use("/teams", teamRoutes);
 
 
 // Server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port: ${port}`);
 })
